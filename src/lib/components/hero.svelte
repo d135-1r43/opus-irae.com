8c7d9c5c29ea3131a0f64a1c5b1a70e1494e6d0e
mhe <markus.herhoffer@exentra.de>
2023-11-26 15:31:54 +0100

    🚧 (wip): Checkpoint before new layout

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,19 +1,22 @@
 <template>
-  <div class="hero min-h-screen">
-    <div class="bg-opacity-45 hero-overlay "></div>
-    <div class="hero-content text-center text-neutral-content">
-      <div class="max-w-md">
-        <h1 class="mb-5 font-poppins text-5xl font-bold">Hello there</h1>
-        <p class="mb-5 font-inter">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
-        </p>
-        <button class="btn btn-primary font-poppins">Get Started</button>
-      </div>
+    <div class="hero min-h-screen">
+        <div class="bg-opacity-45 hero-overlay "></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-3xl">
+                <h1 class="mb-5 mt-10 font-trajan text-2xl tracking-wider">Latest Release</h1>
+                <img class="p-10" alt="Album cover of Into the Endless Night" src="iten.jpg">
+                <p class="mb-5 font-krete tracking-wider">
+                    Embrace the abyss with Opus Irae's debut album "Into the Endless Night." This symphonic black metal
+                    masterpiece defies convention, blending haunting orchestration with relentless aggression. Prepare
+                    to journey through a realm of despair and sacred divinity, where the night's secrets are endless.
+                    Dive into the maw of oblivion and be consumed by Opus Irae's revelation.
+                </p>
+                <p class="mb-5 font-krete font-extralight tracking-wider">
+                    Mixed and mastered by Dan Swanö <br/>
+                    Artwork by Kristian Wählin<br/>
+                </p>
+                <button class="btn btn-primary font-poppins">Get Started</button>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,32 @@
 module.exports = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extends: {},
-		fontFamily: {
-			inter: ['inter', 'sans-serif'],
-			poppins: ['poppins', 'serif'],
-			'fira-mono': ['fira-mono'],
-      trajan: ['trajan-pro-3', 'serif']
-		}
-	},
-	plugins: [require('@tailwindcss/typography'), require('daisyui')],
-	// daisyUI config (optional)
-	daisyui: {
-		themes: [
-			{
-				opusirae: {
-					primary: '#eeeaea',
-					secondary: '#3e8fb0',
-					accent: '#eb6f92',
-					neutral: '#092145',
-					'base-100': '#0C1822',
-					info: '#9ccfd8',
-					success: '#c4a7e7',
-					warning: '#f6c177',
-					error: '#b4637a'
-				}
-			}
-		]
-	}
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extends: {},
+        fontFamily: {
+            inter: ['inter', 'sans-serif'],
+            poppins: ['poppins', 'serif'],
+            'fira-mono': ['fira-mono'],
+            trajan: ['trajan-pro-3', 'serif'],
+			krete: ['krete', 'serif']
+        }
+    },
+    plugins: [require('@tailwindcss/typography'), require('daisyui')],
+    // daisyUI config (optional)
+    daisyui: {
+        themes: [
+            {
+                opusirae: {
+                    primary: '#eeeaea',
+                    secondary: '#3e8fb0',
+                    accent: '#eb6f92',
+                    neutral: '#092145',
+                    'base-100': '#0C1822',
+                    info: '#9ccfd8',
+                    success: '#c4a7e7',
+                    warning: '#f6c177',
+                    error: '#b4637a'
+                }
+            }
+        ]
+    }
 };
