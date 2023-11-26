c236b7a5b5e3df047481d0915ccc0e02f8cda64e
mhe <markus.herhoffer@exentra.de>
2023-11-26 18:22:38 +0100

    🎨 (navitem): Introduce NavItem

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,8 +1,13 @@
-<aside class="h-full flex p-5 pt-4 md:p-2xl items-center overflow-auto bg-amber-400">
-  <div
-    class="w-full md:min-w-[480px] md:max-w-[520px] h-full md:text-white flex flex-col divide-y divide-foreground md:divide-white">
-    <h1
-      class="
+<script lang="ts">
+  import NavItem from '$lib/components/navitem.svelte';
+</script>
+
+<template>
+  <aside class="h-full flex p-5 pt-4 md:p-2xl items-center overflow-auto bg-amber-400">
+    <div
+      class="w-full md:min-w-[480px] md:max-w-[520px] h-full md:text-white flex flex-col divide-y divide-foreground md:divide-white">
+      <h1
+        class="
         p-6 pb-1.5
         md:text-title2
         md:pb-4
@@ -11,14 +16,13 @@
         transition-opacity duration-300
         md:relative md:-left-1
         ">
-      <a href="/">Opus Iræ</a>
-    </h1>
-    <nav class="flex flex-col py-5 z-10"><a
-      class="block text-2xl leading-[55px] font-krete hover:opacity-60 transition-opacity duration-300"
-      href="/music">Music</a><a
-      class="block text-2xl leading-[55px] font-krete hover:opacity-60 transition-opacity duration-300"
-      href="/tour">Tour</a><a
-      class="block text-2xl leading-[55px] font-krete hover:opacity-60 transition-opacity duration-300"
-      href="/info">Info</a></nav>
-  </div>
-</aside>
+        <a href="/">Opus Iræ</a>
+      </h1>
+      <nav class="flex flex-col py-5 z-10">
+        <NavItem text="Discography" href="/music"/>
+        <NavItem text="Live" href="/music"/>
+        <NavItem text="Info" href="/music"/>
+      </nav>
+    </div>
+  </aside>
+</template>
---
+++ src/lib/components/navitem.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let text = 'Default Text'; // Default value is optional
+  export let href = '#'; // Default href value
+</script>
+
+<template>
+  <a class="block text-2xl leading-[55px] font-krete hover:opacity-60 transition-opacity duration-300"
+     href="{href}">
+    {text}
+  </a>
+</template>
