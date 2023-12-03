96eadd29f5c5f2a7c0188ce645dbec3ddb5c23fc
mhe <markus.herhoffer@exentra.de>
2023-12-03 18:27:06 +0100

    ✨ (hepta): Add heptagram icon

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -2,22 +2,24 @@
   import type { PageData } from "./$types";
   import { _getImageUrl } from "./+page";
 
+  import Heptagram from "$lib/components/icons/heptagram.svelte";
+
   export let data: PageData;
 
   function formatDate(datestring: string): string {
     let date: Date = new Date(datestring);
 
-    var duration = +(new Date()) - +date;
+    const duration = +(new Date()) - +date;
     if (duration < 1000 * 60 * 60 * 24 * 365) {
       // less than a year ago
-      var prefix: string = (duration < 0) ? "Release: " : "";
+      const prefix: string = (duration < 0) ? "Release: " : "";
       return prefix + date.toLocaleDateString(undefined, { month: "long", day: "numeric", year: "numeric" });
     } else {
+      // more than a year ago
       return date.getFullYear().toString();
     }
   }
 </script>
-
 
 <svelte:head>
   <title>Opus Iræ Discography</title>
@@ -25,7 +27,7 @@
 
 <template>
   <aside class="
-      h-full w-full pt-4
+      w-full pt-4
       md:ml-10 md:p-2xl md:min-w-[480px] md:max-w-[820px] md:text-white md:divide-white
       flex-col divide-y
       bg-black bg-transparent/30
@@ -42,18 +44,13 @@
         md:relative md:-left-1">
       <a href="/">Opus Iræ</a>
     </h1>
-    <!-- nav class="flex flex-col py-5 pl-7">
-      <NavItem text="Discography" href="/music"/>
-      <NavItem text="Live" href="/"/>
-      <NavItem text="Info" href="/"/>
-    </nav -->
 
     <div class="flex flex-col px-5 grow">
       {#each data.releases.data as { title, type, release_date, label, cover }}
         <div class="space-y-xl px-0 py-5 md:p-5">
-          <a class="block space-y-3 group" href="/music/the-burden-of-man">
+          <a class="block space-y-3 group" href="/">
             <img
-              alt="The Burden of Man" width="500" height="500" decoding="async" data-nimg="1"
+              alt="{title} Cover" width="500" height="500" decoding="async" data-nimg="1"
               class="w-full aspect-square object-cover rounded group-hover:scale-[.99] group-hover:brightness-75 transition-transform-filter ease-in-out duration-500"
               style="color: transparent;"
               src="{ _getImageUrl(cover)}">
@@ -62,6 +59,9 @@
               <p class="pt-0.5 font-krete text-m">{type} · {formatDate(release_date)} · {label}</p>
             </div>
           </a>
+        </div>
+        <div class="flex justify-center py-10">
+          <Heptagram/>
         </div>
       {/each}
     </div>
