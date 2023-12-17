f3973615dadd2fead740b6dd28a57c12deabb560
mhe <markus.herhoffer@exentra.de>
2023-12-17 16:59:47 +0100

    🐛 (disco): Only show published releases

--- src/routes/music/+page.ts
+++ src/routes/music/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 	const res: Response = await fetch(
-		`https://directus.herhoffer.net/items/release/?band=1&sort=-release_date`
+		`https://directus.herhoffer.net/items/release/?band=1&sort=-release_date&filter[status][_eq]=published`
 	);
 	const releases = await res.json();
 	return { releases: releases };
