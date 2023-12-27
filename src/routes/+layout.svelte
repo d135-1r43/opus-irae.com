e268daafd0026f060b8f67027d52316fdb79dd5d
mhe <markus.herhoffer@exentra.de>
2023-12-27 21:57:34 +0100

    🔍️ (seo): Background color, use webp

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,7 +29,7 @@
     <img alt="Blue Background with Clouds and Stars" decoding="sync"
          class="md:block object-cover transition-opacity bg-black opacity-95 -z-10"
          style="position: fixed; height: 100%; width: 100%; inset: 0px" sizes="100vw"
-         src="background-kw.png">
+         src="background-kw.webp">
     <slot />
   </div>
 </QueryClientProvider>
