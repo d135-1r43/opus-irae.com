c0f9d9bdfe2b725b588f102046f8fc71ef4c4758
mhe <markus.herhoffer@exentra.de>
2023-12-28 22:24:57 +0100

    🎨 (format): Reformat footer component code
    The footer code in Svelte component has been reformatted for better readability. Indentation has been adjusted to bring clarity to the structure, without changing any functionality.

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -6,12 +6,12 @@
 <template>
   <div class="fixed bottom-0 border-none">
     <footer class="mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between md:pl-10 font-krete z-20">
-  <span class="text-xs text-gray-600 sm:text-center">
-    © {currentYear} Opus Iræ & Endtime Productions AB
-  </span>
+      <span class="text-xs text-gray-600 sm:text-center">
+        © {currentYear} Opus Iræ & Endtime Productions AB
+      </span>
       <span class="flex flex-wrap items-center mt-3 text-xs font-medium text-gray-600 sm:mt-0">
-      <a href="impress" class="mr-2 hover:underline md:mr-2 md:ml-10">Impress & Privacy Policy</a>
-  </span>
+        <a href="impress" class="mr-2 hover:underline md:mr-2 md:ml-10">Impress & Privacy Policy</a>
+      </span>
     </footer>
   </div>
 </template>
