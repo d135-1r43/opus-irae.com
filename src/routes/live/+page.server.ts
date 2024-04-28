3a2695e2555bcf1c4a362e0085466b528d8907e0
mhe <markus.herhoffer@exentra.de>
2024-04-28 16:42:39 +0200

    🐛 (live) Fix TypeScript ignores and add past events sort
    The commit introduces `@ts-ignore` to suppress TypeScript errors in 'src/routes/live/+page.server.ts'. Additionally, the functionality to sort past events based on date has been added.

--- src/routes/live/+page.server.ts
+++ src/routes/live/+page.server.ts
@@ -1,5 +1,7 @@
+// @ts-ignore
 import type { PageLoad } from './$types';
 
+// @ts-ignore
 export const load: PageLoad = async ({ fetch }) => {
 	const eventsRes: Response = await fetch(
 		`https://directus.herhoffer.net/items/band/1?fields=events.*`
@@ -12,6 +14,10 @@ export const load: PageLoad = async ({ fetch }) => {
 	const pastEvents = events.data.events.filter((event: { date: string }): boolean => {
 		const eventDate: Date = new Date(event.date);
 		return eventDate < today;
+	});
+
+	pastEvents.sort((a: { date: string }, b: { date: string }): number => {
+		return new Date(b.date).getTime() - new Date(a.date).getTime();
 	});
 
 	const futureEvents = events.data.events.filter((event: { date: string }): boolean => {
