ed19cc6d4f476e01f515d6964ea07f9d5eb93d07
Markus Herhoffer <markus@herhoffer.net>
2026-01-05 14:25:50 +0100

    🐛 (directus): Add band filter for fetching releases
    Added a new API configuration to fetch and filter music releases. Updated server logic and associated files to align with the new endpoint structure.

--- src/routes/music/+page.server.ts
+++ src/routes/music/+page.server.ts
@@ -4,7 +4,7 @@ import type { PageLoad } from './$types';
 // @ts-ignore
 export const load: PageLoad = async ({ fetch }) => {
 	const res: Response = await fetch(
-		`https://directus.herhoffer.net/items/release/?band=1&sort=-release_date&filter[status][_eq]=published`
+		`https://directus.herhoffer.net/items/release/?sort=-release_date&filter[status][_eq]=published&filter[band][_eq]=1`
 	);
 	const releases = await res.json();
 	return { releases: releases };
